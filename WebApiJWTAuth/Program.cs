9db3538bb1b725450dfc74bbfd131915be17d953
ykw <ykw@myfastrack.net>
2025-05-30 15:05:08 +0800

    enable swagger.

--- WebApiJWTAuth/Program.cs
+++ WebApiJWTAuth/Program.cs
@@ -90,7 +90,7 @@ builder.Services.AddSwaggerGen(setup =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
